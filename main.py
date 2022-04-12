9bda84514bd2c18d609ae330e6d7974e52deb1a5
ChenXiao Liu <810196813@qq.com>
2022-04-12 16:21:20 -0500

    complete all model

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
+# https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
 from sklearn.decomposition import PCA
+# https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.SequentialFeatureSelector.html#sklearn.feature_selection.SequentialFeatureSelector
 from sklearn.feature_selection import SequentialFeatureSelector
-from sklearn.feature_selection import SelectFromModel
+# https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.SelectKBest.html#sklearn.feature_selection.SelectKBest
+from sklearn.feature_selection import SelectKBest, chi2
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('letter-recognition.data', header=None).to_numpy()
@@ -25,8 +28,14 @@ def split_data(samples):
 
 
 def fit(model, name, dataset, classifier, hyperparameter, x_label, dr_method, dr_require_y=True):
+    values = list(hyperparameter.values())[0]
+    _min = min(values)
+    _max = max(values)
+    interval = (_max - _min) / (len(values) - 1)
+    _range = np.arange(_min - interval, _max + interval, interval)
+
     def plot(data_type):
-        plt.xticks(np.arange(0, 6, 1))
+        plt.xticks(_range)
         plt.scatter(list(hyperparameter.values())[0], clf.cv_results_['mean_test_score'])
         plt.title(f"{model}_{data_type}_{name}")
         plt.xlabel(x_label)
@@ -49,7 +58,7 @@ def fit(model, name, dataset, classifier, hyperparameter, x_label, dr_method, dr
     # print(clf.cv_results_)
     # print(clf.cv_results_['params'])
     # plt.plot(raw[:, column], predicted, color="yellow")
-    # dr_x = np.array()
+    # dr = dimension reduction
     if dr_require_y:
         dr_x = dr_method.fit_transform(x, y)
     else:
@@ -65,21 +74,18 @@ def train_models(dataset, name):
         PCA(n_components=4), False)
     # https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
     fit('decision_tree', name, dataset, DecisionTreeClassifier(), {'max_depth': [1, 2, 3, 4, 5]}, 'max_depth',
-        SequentialFeatureSelector(KNeighborsClassifier(n_neighbors=3), n_features_to_select=4))
+        SelectKBest(k=4))
     # https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
     fit('random_forest', name, dataset, RandomForestClassifier(), {'max_depth': [1, 2, 3, 4, 5]}, 'max_depth',
         SequentialFeatureSelector(KNeighborsClassifier(n_neighbors=3), n_features_to_select=4, direction='backward'))
+    # https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
+    fit('svm', name, dataset, SVC(), {'C': [1, 2, 3, 4, 5]}, 'C',
+        SequentialFeatureSelector(KNeighborsClassifier(n_neighbors=3), n_features_to_select=4))
+    # https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html#sklearn.neural_network.MLPClassifier
+    fit('ann', name, dataset, MLPClassifier(max_iter=1500), {'hidden_layer_sizes': [100, 150, 200, 250, 300]},
+        'hidden_layer_sizes', SelectKBest(chi2, k=4))
 
 
 train_models(data_hk, 'HK')
 train_models(data_my, 'MY')
 train_models(data_ab, 'AB')
-# # https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
-# train_model(data_hk, SVC(), {'kernel': ('linear', 'poly', 'rbf', 'sigmoid')})
-# # https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html#sklearn.neural_network.MLPClassifier
-# train_model(data_hk, MLPClassifier(max_iter=1000), {'learning_rate': ['constant', 'invscaling', 'adaptive']})
-
-# # https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
-# # https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.SequentialFeatureSelector.html#sklearn.feature_selection.SequentialFeatureSelector
-# # https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.SelectFromModel.html#sklearn.feature_selection.SelectFromModel
-# print(SelectFromModel(estimator=DecisionTreeClassifier()).fit_transform(X, y))
